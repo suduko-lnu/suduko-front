a29a7ca9ff92b677ce55cee5bbef42294aaa9f47
Andrii Mudryk <qwed2297@gmail.com>
2020-03-15 17:41:21 +0200

    first commit

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+
+import './../node_modules/bulma/css/bulma.css';
+
+import App from './App.vue';
+import router from './router';
+import store from './store';
+
+Vue.use(Antd);
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+    <section class="home hero is-fullheight">
+    	<div class="hero-body">
+    		<div class="container">
+    			<h1 class="title">
+    				text
+    			</h1>
+    			<h2 class="subtitle">
+    				subtext
+    			</h2>
+    		</div>
+    	</div>
+    </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import HelloWorld from '@/components/HelloWorld.vue'
+
+@Component({
+	components: { HelloWorld }
+})
+
+export default class Home extends Vue {
+  
+}
+</script>
+
+<style>
+</style>
